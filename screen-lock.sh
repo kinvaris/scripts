92baa9e3c4d367e98a1628847f2c9eeb6e02b951
Jonas Libbrecht <jonas.libbrecht@yahoo.com>
2017-03-31 13:21:24 +0200

    Update screen-lock.sh

--- screen-lock.sh
+++ screen-lock.sh
@@ -4,6 +4,6 @@
 
 # VERSION 1
 scrot /tmp/lockscreen.png
-gm convert /tmp/lockscreen.png -blur 0x5 /tmp/lockscreen.png
+convert /tmp/lockscreen.png -blur 0x5 /tmp/lockscreen.png
 i3lock -i /tmp/lockscreen.png
 sleep 60; pgrep i3lock && xset dpms force off
