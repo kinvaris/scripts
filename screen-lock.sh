7a146a6d345294572cfc9c2f8357b5aae5699402
Jonas Libbrecht <jonas.libbrecht@yahoo.com>
2017-03-31 13:06:23 +0200

    Create screen-lock.sh

---
+++ screen-lock.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Required packages: scrot i3lock imagemagick
+
+# VERSION 1
+scrot /tmp/lockscreen.png
+gm convert /tmp/lockscreen.png -blur 0x5 /tmp/lockscreen.png
+i3lock -i /tmp/lockscreen.png
+sleep 60; pgrep i3lock && xset dpms force off
